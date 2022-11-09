4bde0aa5f9f9436d6bdb2e546b4d7f6c56a88e46
Andros Fenollosa <andros@fenollosa.email>
2022-11-09 12:48:50 +0100

    Add setup and ignores

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
   keywords = ['database', 'immutable', 'nosql', 'json'],
   classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU License",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
   ),
   install_requires=[],
